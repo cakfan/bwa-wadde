bff176ce28026d98d04a2c3c5865aa616ce32757
cakfan <taufan.fhm8@gmail.com>
2025-03-17 10:28:51 +0700

    fix: hero background

--- src/components/header/BackgroundHeader.tsx
+++ src/components/header/BackgroundHeader.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export const BackgroundHeader = () => {
   return (
-    <div className="absolute -right-[550px] -z-10 top-9 w-[1343px] h-[768px] bg-background">
+    <div className="absolute left-[674px] -z-10 top-9 w-[1414px] h-[900.11] bg-background">
       <Image
         src={`${
           process.env.NODE_ENV === "production" ? "/bwa-wadde/" : "/"
@@ -10,7 +10,7 @@ export const BackgroundHeader = () => {
         alt="background header"
         width={1343}
         height={768}
-        className="w-full h-full object-cover mix-blend-soft-light"
+        className="w-full h-full mix-blend-soft-light"
       />
     </div>
   );
